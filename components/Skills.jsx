641fed099c9d5cd25ab2e727e631b3f2ee9ea01a
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-02-22 12:41:38 +0530

    remove unwanted code

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -13,7 +13,6 @@ import Typescript from "../public/assets/skills/typescript_original_logo_icon_14
 import Mongodb from "../public/assets/skills/mongo.png";
 import Sass from "../public/assets/skills/sass_original_logo_icon_146350.png";
 import Redux from "../public/assets/skills/redux_original_logo_icon_146365.png";
-import Docker from "../public/assets/skills/docker_original_logo_icon_146556.png";
 import Git from "../public/assets/skills/git_original_logo_icon_146509.png";
 import Botstrap from "../public/assets/skills/bootstrap_plain_logo_icon_146619.png";
 
@@ -153,16 +152,6 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>Redux</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Docker} width="64px" height="64px" alt="/" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Docker</h3>
               </div>
             </div>
           </div>
